524e755b66905539931f68822481695464e0a8bb
Juliana Yuki Okubo <ra177320@gambit.lab.ic.unicamp.br>
2019-09-30 20:14:29 -0300

    Tratamento processos zumbis

--- servidor.c
+++ servidor.c
@@ -4,27 +4,34 @@
 #include <string.h>
 #include <netdb.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <time.h>
+#include <signal.h>
 
 #define MAXDATASIZE 20000
+
+void err_sys(const char* x)
+{
+    perror(x);
+    exit(1);
+}
+
 
 int Socket(int family, int type, int flags) {
    int sockfd;
    if ((sockfd = socket(family, type, flags)) < 0) {
-      perror("socket");
-      exit(1);
+      err_sys("socket");
    } else
       return sockfd;
 }
 
 void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
    if (bind(sockfd, addr, addrlen) == -1) { // "Associa um nome a um socket"
-      perror("bind");
-      exit(1);
+      err_sys("bind");
    }
 }
 
@@ -32,16 +39,17 @@ int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
     //accept connection from an incoming client
     int acceptedfd = accept(sockfd, addr, addrlen);
     if (acceptedfd < 0) {
-    	perror("accept");
-    	exit(1);
+      if(errno != EINTR){
+        err_sys("accept error");
+      }
     }
     return acceptedfd;
 }
 
 void Listen(int sockfd, int backlog) {
     if (listen(sockfd, backlog) == -1) {
-        perror("listen");
-        exit(1);
+        err_sys("listen");
+
     }
 }
 
@@ -58,8 +66,7 @@ void Log(char *ip, int port, int state) {
     FILE *fptr;
     fptr = fopen("server_log.txt", "a");
     if(fptr == NULL){
-      printf("Error!");
-      exit(1);
+      err_sys("Error!");
     }
 
     if (state == 0)
@@ -71,6 +78,37 @@ void Log(char *ip, int port, int state) {
 
 void Print(char *ip, int port, char *buf) {
     printf("Command from %s:%d ->\n %s\n", ip, port, buf);
+}
+
+typedef void Sigfunc(int);
+
+Sigfunc * Signal(int signo, Sigfunc *func){
+  struct sigaction act, oact;
+  act.sa_handler = func;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
+  if(signo == SIGALRM){
+#ifdef SA_INTERRUPT
+    act.sa_flags |= SA_INTERRUPT;
+#endif
+  }else{
+#ifdef SA_RESTART
+    act.sa_flags |= SA_RESTART;
+#endif
+  }
+  if(sigaction (signo, &act, &oact) < 0){
+    return(SIG_ERR);
+  }
+  return (oact.sa_handler);
+}
+
+void sig_chld(int signo){
+  pid_t pid;
+  int stat;
+  while ((pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+    printf("child %d terminated\n", pid);
+  }
+  return;
 }
 
 int main (int argc, char **argv) {
@@ -103,71 +141,70 @@ int main (int argc, char **argv) {
 
     //Queue size been passed
     Listen(listenfd, atoi(argv[2]));
-
+    Signal (SIGCHLD, sig_chld);
     printf("Awaiting for clients...\n");
 
     while (1) {
     	bzero(&clientaddr, sizeof(clientaddr));
-	sleep(20);
-        connfd = Accept(listenfd, (struct sockaddr *) &clientaddr, &c);
+      sleep(2);
+      connfd = Accept(listenfd, (struct sockaddr *) &clientaddr, &c);
 
     	pV4Addr = (struct sockaddr_in *)&clientaddr;
     	ipAddr = pV4Addr->sin_addr;
     	inet_ntop(AF_INET, &ipAddr, address, INET_ADDRSTRLEN);
-        port = ntohs(pV4Addr->sin_port);
-        printf("Connection accepted -> %s:%d\n", address, port);
-        Log(address, port, 0);
-        if ( (pid = fork()) == 0) {
-            Close(listenfd);
+      port = ntohs(pV4Addr->sin_port);
+      printf("Connection accepted -> %s:%d\n", address, port);
+      Log(address, port, 0);
+      if ( (pid = fork()) == 0) {
+          Close(listenfd);
 
-            while( (read_size = read(connfd , buf , MAXDATASIZE)) > 0 ) {
-                buf[read_size] = 0;
-                Print(address, port, buf);
-                // printf("%s\n", buf);
-                /* Open the command for reading. */
-                fp = popen(buf, "r");
-                if (fp == NULL) {
-                    printf("Failed to run command\n" );
-                    Close(connfd);
-                    exit(1);
+          while( (read_size = read(connfd , buf , MAXDATASIZE)) > 0 ) {
+              buf[read_size] = 0;
+              Print(address, port, buf);
+              // printf("%s\n", buf);
+              /* Open the command for reading. */
+              fp = popen(buf, "r");
+              if (fp == NULL) {
+                  printf("Failed to run command\n" );
+                  Close(connfd);
+                  exit(1);
+              }
+
+              /* Read the output a line at a time - output it. */
+              while (fgets(path, sizeof(path)-1, fp) != NULL) {
+                strcat(outt, path);
+              }
+
+              /* close */
+              int status = pclose(fp);
+              if(status==0){
+                if(outt[0]=='\0'){
+                  outt[0] = ' ';
                 }
-
-                /* Read the output a line at a time - output it. */
-                while (fgets(path, sizeof(path)-1, fp) != NULL) {
-                  strcat(outt, path);
+              }else{
+                if(outt[0]=='\0'){
+                  buf[strcspn(buf, "\n")] = 0;
+                  strcpy(outt, strcat(buf, ": not found"));
                 }
+              }
+              write(connfd , outt , strlen(outt));
+              memset(outt, 0, strlen(outt));
 
-                /* close */
-                int status = pclose(fp);
-                if(status==0){
-                  if(outt[0]=='\0'){
-                    outt[0] = ' ';
-                  }
-                }else{
-                  if(outt[0]=='\0'){
-                    buf[strcspn(buf, "\n")] = 0;
-                    strcpy(outt, strcat(buf, ": not found"));
-                  }
-                }
-                write(connfd , outt , strlen(outt));
-                memset(outt, 0, strlen(outt));
+              path[0] = '\0';
+          }
 
-                path[0] = '\0';
-            }
-
-            if(read_size == 0) {
-                Log(address, port, 1);
-                pV4Addr = (struct sockaddr_in *)&clientaddr;
-                ipAddr = pV4Addr->sin_addr;
-                inet_ntop(AF_INET, &ipAddr, address, INET_ADDRSTRLEN);
-                printf("Client disconnected -> %s:%d\n", address, ntohs(pV4Addr->sin_port));
-                fflush(stdout);
-            }
-            Close(connfd);
-            exit(0);
-        }
-      
-        Close(connfd);
+          if(read_size == 0) {
+              Log(address, port, 1);
+              pV4Addr = (struct sockaddr_in *)&clientaddr;
+              ipAddr = pV4Addr->sin_addr;
+              inet_ntop(AF_INET, &ipAddr, address, INET_ADDRSTRLEN);
+              printf("Client disconnected -> %s:%d\n", address, ntohs(pV4Addr->sin_port));
+              fflush(stdout);
+          }
+          Close(connfd);
+          exit(0);
+      }
+      Close(connfd);
     }
 
    return 0;
